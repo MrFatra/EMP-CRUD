0dfeb6f1225777f4e078bfac48c3b85c88454da9
MrFatra <ramadhanfatra2004@gmail.com>
2024-02-06 07:43:47 +0700

    (chore): update-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,30 @@
+'use client'
 import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/Icons";
+import { title } from "@/components/primitives";
+import { GithubIcon, InstagramIcon } from "@/components/Icons";
+import { FaArrowRight } from 'react-icons/fa6'
 
 export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2>
+			<div className="max-w-lg justify-center px-5">
+				<h1 className={title({ color: "cyan" })}>Heloo!</h1>
+				<h1 className={title()}>Welcome to my project!</h1>
+				<p className="my-5 text-xl text-center font-medium">This project is built with a Next.js starter with TypeScript and Tailwind CSS + NextUI Template.</p>
+				<p className="mb-10 text-medium font-medium">I'd love it if you'd join me in completing or collaborating on this project.</p>
+				<p className="text-center">Please grab this button & checkout my Github or Instagram profile!😉</p>
 			</div>
 
 			<div className="flex gap-3">
 				<Link
 					isExternal
-					href={siteConfig.links.docs}
+					href={siteConfig.links.instagram}
 					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
 				>
-					Documentation
+					<InstagramIcon size={26} />
+					Instagram
 				</Link>
 				<Link
 					isExternal
@@ -37,14 +34,18 @@ export default function Home() {
 					<GithubIcon size={20} />
 					GitHub
 				</Link>
+				<Link
+					className={buttonStyles({ variant: "bordered", radius: "full", color: 'primary' })}
+					href={'/employees'}
+				>
+					Employees
+					<FaArrowRight/>
+				</Link>
 			</div>
 
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
+			<div className="text-center mt-10 text-foreground-500">
+				<p>Email me at: </p>
+				<p>{siteConfig.links.email}</p>
 			</div>
 		</section>
 	);
